40f3a01099d2180b43ff8d9cbef5cdd6cf1f00ae
sos1819jmp <josmorper11@alum.us.es>
2019-03-18 11:38:24 +0000

    Prueba 3

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use(bodyParser.json());
 app.use("/", express.static(__dirname + "/public")); // __dircountry equivale a la ruta raiz donde se esta ejecutando el jnode
 
 app.use("/api/v1/companies", express.static(__dirname + "/public/companies.html"));
+app.use("/api/v1/companies", require('./api/v1/companies'));
 
 app.get("/time", (request, response) => {
     response.send(new Date());
