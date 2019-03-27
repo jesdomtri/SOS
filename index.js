9b24e5ec237b0702584f70187d0f1baa99fa584b
sos1819jpm <joapuemar1@alum.us.es>
2019-03-27 13:30:30 +0000

    f05

--- index.js
+++ index.js
@@ -446,7 +446,7 @@ app.put("/api/v1/computers-attacks-stats/:country", (req, res) => {
     var country = req.params.country;
     var updatedStats = req.body;
 
-    var keys = ["country", "year"];
+    var keys = ["country", "year","attacktype","economicimpactmillions","affectedequipments","overallpercentage"];
 
     for (var i = keys.length - 1; i--;) {
         if (!updatedStats.hasOwnProperty(keys[i])) {
