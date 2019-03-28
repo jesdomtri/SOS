ef3fd6a3bad5e82c706b52b4988388bdfdafcf51
sos1819jmp <josmorper11@alum.us.es>
2019-03-28 10:33:32 +0000

    Entregable

--- index.js
+++ index.js
@@ -250,12 +250,51 @@ app.get("/api/v1/country-stats/loadInitialData", (req, res) => {
 });
 
 //GET /country-stats/
-app.get("/api/v1/country-stats", (req, res) => {
-    stats.find({}).toArray((error, companiesArray) => {
-        res.send(companiesArray);
-    });
-});
+    app.get("/api/v1/country-stats", (req, res) => {
+    var query = {};
+    let offset = 0;
+    let limit = Number.MAX_SAFE_INTEGER;
 
+    if (req.query.offset) {
+        offset = parseInt(req.query.offset);
+        delete req.query.offset;
+    }
+    if (req.query.limit) {
+        limit = parseInt(req.query.limit);
+        delete req.query.limit;
+    }
+
+    Object.keys(req.query).forEach((i) => {
+        if (isNaN(req.query[i]) == false) {
+            query[i] = parseInt(req.query[i]);
+        }
+        else {
+            query[i] = req.query[i];
+        }
+    });
+
+    if (Object.keys(req.query).includes("from") && Object.keys(req.query).includes("to")) {
+        delete query.from;
+        delete query.to;
+        query["country"] = { "$lte": parseInt(req.query["to"]), "$gte": parseInt(req.query["from"]) };
+    }
+    else if (Object.keys(req.query).includes('from')) {
+        delete query.from;
+        query["country"] = { "$gte": parseInt(req.query["from"]) };
+    }
+    else if (Object.keys(req.query).includes("to")) {
+        delete query.to;
+        query["country"] = { "$lte": parseInt(req.query["to"]) };
+    }
+
+    stats.find(query).skip(offset).limit(limit).toArray((error, statsArray) => {
+        if (error) {
+            console.log("Error: " + error);
+        }
+        else {
+            res.send(statsArray);
+        }
+    });
 //GET /country-stats/País
 app.get("/api/v1/country-stats/:country", (req, res) => {
     var country = req.params.country;
