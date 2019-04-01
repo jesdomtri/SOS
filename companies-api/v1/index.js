5445141e08e76c0dc340f3bb9c2939c3d67dccca
sos1819jdt <jesdomtri@gmail.com>
2019-04-01 14:07:56 +0000

    update jesus

--- companies-api/v1/index.js
+++ companies-api/v1/index.js
@@ -171,8 +171,8 @@ module.exports = function(app, companies, BASE_PATH) {
                 console.log("Error: " + error);
             }
             else {
-                res.send(filteredcompanies);
                 companies.updateOne({ "country": country, "year": parseInt(year) }, { $set: updatedCompany });
+                res.send(filteredcompanies);
                 res.sendStatus(200);
             }
         });
