c5bc5b4ff8420a00a97585daf46ca6e281a07fa1
sos1819jdt <jesdomtri@gmail.com>
2019-04-03 08:57:35 +0000

    arreglos de ultima hora

--- index.js
+++ index.js
@@ -43,12 +43,12 @@ client.connect(error => {
         attacks = clientJoaquin.db("Sos-1819-JMP").collection("computers-attacks-stats");
 
         console.log("Connected to database Joaquín .");
-        
+
         const MongoClient = require('mongodb').MongoClient;
         const urlAntonio = "mongodb+srv://root:root@sos1819-jmp-dhhii.mongodb.net/test?retryWrites=true";
         const clientAntonio = new MongoClient(urlAntonio, { useNewUrlParser: true });
-          
-          clientAntonio.connect(error => {
+
+        clientAntonio.connect(error => {
 
             stats = clientAntonio.db("sos1819-03").collection("country-stats");
 
@@ -57,14 +57,14 @@ client.connect(error => {
             app.listen(port, () => {
                 console.log("Server is ready!!");
             }); // Debe dejarse la conexión a la aplicacón dentro ya que si queda afuera puede que se inicie la aplicación pero no la base de datos .
-            
+
             countrystatsAPI(app, stats, BASE_PATH);
 
         });
-        
+
         attacksAPI(app, attacks, BASE_PATH);
     });
 
     companiesAPI(app, companies, BASE_PATH);
-    
+
 });
