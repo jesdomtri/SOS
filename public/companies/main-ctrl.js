247c6048f82a3cafd87aff5faa29237d0156ca91
sos1819jdt <jesdomtri@gmail.com>
2019-04-04 15:11:51 +0000

    Creacion de postman

--- public/companies/main-ctrl.js
+++ public/companies/main-ctrl.js
@@ -2,7 +2,7 @@
 var app = angular.module("PostmanApp");
 app.controller("MainCtrl", ["$scope", "$http", function($scope, $http) {
     console.log("Modular MainCtrl initialized");
-    $scope.url = "/api/v1/contacts";
+    $scope.url = "/api/v1/companies";
     $scope.send = function() {
         $http.get($scope.url).then(function(response) {
             $scope.data = JSON.stringify(response.data, null, 2);
