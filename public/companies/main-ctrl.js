259a2629082b74e3717da0ee306f5bb400e5d60b
sos1819jdt <jesdomtri@gmail.com>
2019-04-07 21:47:01 +0000

    update jesus

--- public/companies/main-ctrl.js
+++ public/companies/main-ctrl.js
@@ -8,4 +8,24 @@ app.controller("MainCtrl", ["$scope", "$http", function($scope, $http) {
             $scope.data = JSON.stringify(response.data, null, 2);
         });
     }
+    $scope.send = function() {
+        $http.get($scope.url).then(function(response) {
+            $scope.data = JSON.stringify(response.data, null, 2);
+        });
+    }
+    $scope.del = function() {
+        $http.delete($scope.url).then(function(response) {
+            $scope.data = JSON.stringify(response.data, null, 2);
+        });
+    }
+    $scope.post = function() {
+        $http.post($scope.url).then(function(response) {
+            $scope.data = JSON.stringify(response.data, null, 2);
+        });
+    }
+    $scope.put = function() {
+        $http.put($scope.url).then(function(response) {
+            $scope.data = JSON.stringify(response.data, null, 2);
+        });
+    }
 }]);
