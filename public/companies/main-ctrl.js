8f6fb63e06c064739152f273955d8b375cb6311c
sos1819jdt <jesdomtri@gmail.com>
2019-04-08 13:46:20 +0000

    update jesus

--- public/companies/main-ctrl.js
+++ public/companies/main-ctrl.js
@@ -31,7 +31,7 @@ app.controller("MainCtrl", ["$scope", "$http", function($scope, $http) {
         });
     }
     $scope.put = function() {
-        $http.put($scope.url, {
+        $http.put($scope.url + "/" + $scope.country + "/" + $scope.year, {
             country: $scope.country,
             year: parseInt($scope.year),
             numberOfCompanies: parseInt($scope.numberOfCompanies),
