07235b8ddbf9d1045c1908481ecdb8e1cdc8d687
sos1819jdt <jesdomtri@gmail.com>
2019-04-08 13:48:23 +0000

    update jesus

--- public/companies/main-ctrl.js
+++ public/companies/main-ctrl.js
@@ -3,7 +3,7 @@ var app = angular.module("PostmanApp");
 app.controller("MainCtrl", ["$scope", "$http", function($scope, $http) {
     console.log("Modular MainCtrl initialized");
     $scope.url = "/api/v1/companies";
-    $scope.send = function() {
+    $scope.get = function() {
         $http.get($scope.url).then(function(response) {
             $scope.data = JSON.stringify(response.data, null, 2);
         });
