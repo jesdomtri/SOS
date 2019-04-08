266e6087371401d8bfb7424d21fc61a5a7a6de52
sos1819jdt <jesdomtri@gmail.com>
2019-04-08 09:23:50 +0000

    update jesus

--- public/companies/main-ctrl.js
+++ public/companies/main-ctrl.js
@@ -19,9 +19,9 @@ app.controller("MainCtrl", ["$scope", "$http", function($scope, $http) {
         });
     }
     $scope.post = function() {
-        $http.post($scope.url + "/" + $scope.country + "/" + $scope.year + "/" + $scope.numberOfCompanies + "/" + $scope.sector + "/" + $scope.page).then(function(response) {
-            //$scope.push = { "Country": $scope.country, "Year": $scope.year, "numberOfCompanies": $scope.numberOfCompanies,
-            //"sector": $scope.sector, "page": $scope.page};
+        $http.post($scope.url).then(function(response) {
+            $scope.push = { "Country": $scope.country, "Year": $scope.year, "numberOfCompanies": $scope.numberOfCompanies,
+            "sector": $scope.sector, "page": $scope.page};
             $scope.data = JSON.stringify(response.data, null, 2);
 
         });
