33677213e91bdd1a7e8f082a2172a5b145a91e65
sos1819jpm <joapuemar1@alum.us.es>
2019-04-09 10:16:18 +0000

    front

--- public/computer-attacks-stats/main-ctrl.js
+++ public/computer-attacks-stats/main-ctrl.js
@@ -1,28 +1,102 @@
-/*global angular $scope*/
-var app = angular.module("PostmanApp");
+    
+    /*global angular $scope*/
+    
+    var app = angular.module("PostmanApp");
+  
     app.controller("MainController",["$scope","$http", function ($scope,$http) {
         
         console.log("MainController initialized");
         
-        $scope.url= "/api/v1/computers-attacks-stats";
+        $scope.url = "/api/v1/computers-attacks-stats";
         
         $scope.get = function(){
+           
             $http.get($scope.url).then( function(response){
+             
                 $scope.data=JSON.stringify(response.data,null,2);
                 $scope.status = JSON.stringify(response.status,null,2);
+          
             }
                 );
         };
         
+        $scope.loadinitial= function(){
+         $http.loadinitial($scope.url + "/loadInitialData").them(function(response){
+             
+             $scope.data=JSON.stringify(response.data,null,2);
+             $scope.status=JSON.stringify(response.status,null,2);
+         
+             
+         } , function ( response){
+           
+             $scope.data = response.data || "Request failed";
+             $scope.status=response.status ; 
+        
+         })   ;
+        
+        }
+       
         $scope.delete = function(){
+           
             $http.delete($scope.url).then(function(response){
                 $scope.data = JSON.stringify(response.data,null,2);
                 $scope.status=JSON.stringify(response.stats,null,2);
                 
+          
             } , function (response){
-                $scope.data=response.data || "Request filed";
+               
+                $scope.data=response.data || "Request failed";
                 $scope.status = response.status
+           
             });
+        };
+      
+        
+        $scope.post = function() { 
+            
+            $http.post(
+                $scope.url,{
+                    country : $scope.country,
+                    year : parseInt($scope.year),
+                    attacktype: $scope.attacktype,
+                    economicimpactmillions: parseFloat($scope.economicimpactmillions),
+                    affectedequipments:  parseInt($scope.affectedequipments),
+                    overallpercentage: parseFloat($scope.overallpercentage)
+            
+                }).then(function(response){
+                  
+                    $scope.data= JSON.stringify(response.data,null,2);
+                    $scope.status=JSON.stringify(response.status,null,2);
+              
+                },function(response){// esta segunda funcónes para que muestre los fallos si se dan .
+               
+                $scope.data=response.data || "Request failed";
+                $scope.status=response.status;
+               
+                });
+        };
+            
+        
+        $scope.put = function(){
+            
+            $http.put($scope.url,{
+                 country : $scope.country,
+                    year : parseInt($scope.year),
+                    attacktype: $scope.attacktype,
+                    economicimpactmillions: parseFloat($scope.economicimpactmillions),
+                    affectedequipments:  parseInt($scope.affectedequipments),
+                    overallpercentage: parseFloat($scope.overallpercentage)
+            
+            }).them(function(response){
+                
+                $scope.data = JSON.stringify(response.data,null,2);
+                $scope.status = JSON.stringify(response.status,null,2);
+                
+            } , function(response){
+                $scope.data= response.data || "Request failed";
+                $scope.status= response.status;
+            });
+            
         };
         
         
