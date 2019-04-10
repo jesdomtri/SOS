a4ba6e8ce787ce78d567531c93eb1e6589607125
sos1819jmp <josmorper11@alum.us.es>
2019-04-10 12:09:15 +0000

    Prueba

--- public/country-stats/main-ctrl.js
+++ public/country-stats/main-ctrl.js
@@ -46,6 +46,17 @@ app.controller("MainCtrl", ["$scope", "$http", function($scope, $http) {
           $scope.status = response.status;
       });
     }
+    
+    $scope.postJSON = function() {
+        $http.post($scope.url, $scope.data).then(function(response) {
+            $scope.data = "";
+            $scope.status = JSON.stringify(response.status, null, 2);
+        }, function(response) {
+            $scope.data = response.data || 'Request failed';
+            $scope.status = response.status;
+        });
+    }
+    
     $scope.put = function() {
         $http.put($scope.url, {
             country: $scope.country,
@@ -60,5 +71,15 @@ app.controller("MainCtrl", ["$scope", "$http", function($scope, $http) {
           $scope.data = response.data || 'Request failed';
           $scope.status = response.status;
       });
+    }
+    
+     $scope.putJSON = function() {
+        $http.put($scope.url, $scope.data).then(function(response) {
+            $scope.data = "";
+            $scope.status = JSON.stringify(response.status, null, 2);
+        }, function(response) {
+            $scope.data = response.data || 'Request failed';
+            $scope.status = response.status;
+        });
     }
 }]);
