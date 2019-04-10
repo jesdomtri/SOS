768019bae753442d480e983f9b61540ed36c95c2
sos1819jpm <joapuemar1@alum.us.es>
2019-04-10 11:38:08 +0000

    f

--- computers-attacks-stats-api/v1/index.js
+++ computers-attacks-stats-api/v1/index.js
@@ -314,13 +314,7 @@ app.delete(BASE_PATH+"/computers-attacks-stats/:country/:year/:attacktype", (req
        res.sendStatus(405);
         
     });
-     app.put(BASE_PATH+"/computers-attacks-stats/:year/:attacktype",(req,res) =>{
-       
-       
-       res.sendStatus(405);
-        
-    });
-   
+  
     
     app.put(BASE_PATH+"/computers-attacks-stats/:attacktype",(req,res) =>{
        
