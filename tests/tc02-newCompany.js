75958f696c50a42433aae30cf841a8542b3a207c
sos1819jdt <jesdomtri@gmail.com>
2019-05-07 12:31:18 +0000

    j

--- tests/tc02-newCompany.js
+++ tests/tc02-newCompany.js
@@ -1,14 +1,14 @@
-describe("Check if a new contact can be created", function() {
-    it("List grow after the contact creation", function() {
-        browser.get("http://localhost:8080");
+describe("Check if a new company can be created. ", function() {
+    it("List grow after the company creation", function() {
+        browser.get("http://localhost:8080/ui/v1/companies/#!/");
         var initialCompanies = element.all(by.repeater("company in companies"))
             .then(function(initialCompanies) {
                 element(by.model("newCompany.country")).sendKeys("España");
-                element(by.model("newCompany.year")).sendKeys("1");
-                element(by.model("newCompany.numberOfCompanies")).sendKeys("1");
-                element(by.model("newCompany.sector")).sendKeys("1");
-                element(by.model("newCompany.page")).sendKeys("1");
-                element(by.css('[value=="Add"]')).click();
+                element(by.model("newCompany.year")).sendKeys(1);
+                element(by.model("newCompany.numberOfCompanies")).sendKeys(1);
+                element(by.model("newCompany.sector")).sendKeys(1);
+                element(by.model("newCompany.page")).sendKeys(1);
+                element(by.css('[value=="Crear"]')).click();
 
                 element.all(by.repeater("company in companies"))
                     .then(function(finalCompanies) {
