fbad4a08204679e4df563fcd42776232fa3d94e3
sos1819jmp <josmorper11@alum.us.es>
2019-05-15 08:37:26 +0000

    JESUS EL MEJOR

--- tests/tc01-loadDataCompany.js
+++ tests/tc01-loadDataCompany.js
@@ -1,6 +1,6 @@
 describe("Data is loaded", function() {
     it("Should show a bunch of data", function() {
-        browser.get("http://localhost:8080/#!/ui/v1/companies");
+        browser.get("https://sos1819-03.herokuapp.com/#!/ui/v1/companies");
         var companies = element.all(by.repeater("company in companies"));
         expect(companies.count()).toBeGreaterThan(0);
     });
