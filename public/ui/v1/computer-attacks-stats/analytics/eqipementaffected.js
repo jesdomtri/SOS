806b81d684999217e79e37f5504f636dc38e7759
sos1819jpm <joapuemar1@alum.us.es>
2019-06-01 11:22:22 +0000

    f

--- public/ui/v1/computer-attacks-stats/analytics/eqipementaffected.js
+++ public/ui/v1/computer-attacks-stats/analytics/eqipementaffected.js
@@ -68,5 +68,34 @@ controller("AnalyticsCtrlAttack", ["$scope", "$http", "$httpParamSerializer", fu
                 
 });
 
+$http.get(BASE_API_PATH).then(function(response) {
+        google.charts.load('current', { 'packages': ['geochart'] });
+        google.charts.setOnLoadCallback(drawRegionsMap);
+
+        function drawRegionsMap() {
+            var variables = [];
+            variables.push(['Country', 'Número de compañias (2017)']);
+
+            var paises = response.data.map(function(d) { return d.country });
+            var años = response.data.map(function(d) { return d.year });
+            var affected = response.data.map(function(d) { return d.affectedequipments });
+
+            for (var i = 0; i < paises.length; i++) {
+                if (años[i] == 2017) {
+                    variables.push([paises[i], affected[i]]);
+                }
+            }
+            
+            console.log(variables);
+
+            var data = google.visualization.arrayToDataTable(variables);
+            var options = {};
+            var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+            chart.draw(data, options);
+        }
+    })
+
+
+
     
 }]);
