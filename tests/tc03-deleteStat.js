e0ff0abb925da5b1c6fda4e64ab03cb2e0e031df
sos1819jmp <josmorper11@alum.us.es>
2019-06-03 16:27:02 +0000

    Fix tests

--- tests/tc03-deleteStat.js
+++ tests/tc03-deleteStat.js
@@ -1,15 +1,46 @@
-describe("Check if a contact can be deleted", function() {
-    it("List grow after the contact deletion", function() {
+describe("Check if a new stat can be created ", function() {
+    it("List grow after the stat creation", function() {
         browser.get("https://sos1819-03.herokuapp.com/#!/ui/v1/country-stats");
-       var initialStats = element.all(by.repeater("stat in stats"))
-            .then(function(initialStats) {
-                //element(by.name('BorrarStat')).click();
-                element.all(by.css('.btn-danger')).last().click();
-                
-                element.all(by.repeater("stat in stats"))
-                    .then(function(finalStats) {
-                        expect(finalStats.length).toEqual(initialStats.length - 1);
-                    });
-            });
+        check();
+
+        function check() {
+            element
+                .all(by.repeater("stat in stats"))
+                .then(function(initialStats) {
+                    if (initialStats.length < 10) {
+                        element.all(by.css('.btn-danger')).last().click();
+                        element
+                            .all(by.repeater("stat in stats"))
+                            .then(function(finalStats) {
+                                expect(initialStats.length).toBeGreaterThan(finalStats.length);
+                            });
+
+                    }
+                    else if (initialStats.length == 0) {
+                        element(by.name('Retroceder')).click();
+
+                        element.all(by.css('.btn-danger')).last().click();
+
+                        element
+                            .all(by.repeater("stat in stats"))
+                            .then(function(finalStats) {
+
+                                expect(initialStats.length).toBeGreaterThan(finalStats.length);
+
+                            });
+
+                    }
+                    else {
+
+                        element(by.name('Avanzar')).click().then(check());
+
+                    }
+
+                });
+
+        }
+
+
     });
+
 });
