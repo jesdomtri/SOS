2f8a155eee83d0af7cde50b5e463ab9380c32858
sos1819jdt <jesdomtri@gmail.com>
2019-06-03 16:36:56 +0000

    jesus

--- tests/testCompanies.js
+++
@@ -1,5 +0,0 @@
-exports.config = {
-    seleniumAddress: "http://localhost:4444/wd/hub",
-    chromeOnly: true,
-    specs: ["tc01-loadDataCompany.js", "tc02-newCompany.js", "tc03-deleteCompany.js"]
-}
--- tests/testCountryStats.js
+++
@@ -1,5 +0,0 @@
-exports.config = {
-    seleniumAddress: "http://localhost:4444/wd/hub",
-    chromeOnly: true,
-    specs: ["tc01-loadDataStat.js", "tc02-newStat.js", "tc03-deleteStat.js"]
-}
